e843e526e77e316b09d7e9f90ed682c7ff57d523
Przemysław Żebrowski <p.zebrowski30@gmail.com>
2022-05-09 19:27:39 +0200

    XD

---
+++ Quiz/DataBase/DatabaseTools.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Navigation;
+using Newtonsoft.Json;
+using Quiz.MVVM.Model;
+
+namespace Quiz.DataBase
+{
+    public class DatabaseTools
+    {
+        private static readonly string _questionsPath = @"..\..\..\DataBase\questions.json";
+        public List<Question> Questions { get; set; } = LoadQuestions();
+        public static List<Question> LoadQuestions()
+        {
+            using var reader = new StreamReader(_questionsPath, Encoding.Default);
+            var json = reader.ReadToEnd();
+            var questions = JsonConvert.DeserializeObject<List<Question>>(json);
+
+            return questions;
+        }
+
+        public void Save()
+        {
+            var json = JsonConvert.SerializeObject(Questions);
+            File.WriteAllText(_questionsPath, json, Encoding.Default);
+        }
+    }
+}
--- Quiz/MVVM/ViewModel/SettingsViewModel.cs
+++ Quiz/MVVM/ViewModel/SettingsViewModel.cs
@@ -1,13 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Input;
+using Quiz.Core;
+using Quiz.DataBase;
 
 namespace Quiz.MVVM.ViewModel
 {
-    public class SettingsViewModel
+    public class SettingsViewModel : ObservableObject
     {
+        public ICommand ItemChangedCommand;
+        private readonly DatabaseTools _questionTools;
+        public SettingsViewModel(DatabaseTools questionTools)
+        {
+            _questionTools = questionTools;
+            ItemChangedCommand = new RelayCommand(ShowSelectedQuestion);
+        }
 
+        public List<string> QuestionsForView => _questionTools.Questions.Select(q => q.QuestionContent).ToList();
+        public int CurrentIndex { get; set; } = -1;
+        public string Content { get; set; } = string.Empty;
+        public string AnswerA { get; set; } = string.Empty;
+        public string AnswerB { get; set; } = string.Empty;
+        public string AnswerC { get; set; } = string.Empty;
+        public string AnswerD { get; set; } = string.Empty;
+
+        private void ShowSelectedQuestion(object obj)
+        {
+            var question = _questionTools.Questions[CurrentIndex];
+            Content = question.QuestionContent;
+            AnswerA = question.AnswerA;
+            AnswerB = question.AnswerB;
+            AnswerC = question.AnswerC;
+            AnswerD = question.AnswerD;
+        }
     }
 }
