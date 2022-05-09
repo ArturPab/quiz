a9d25aa459b1c1a5f043342ed9a7533560faa3fa
Przemysław Żebrowski <p.zebrowski30@gmail.com>
2022-05-09 20:05:12 +0200

    lalal

--- Quiz/MVVM/ViewModel/SettingsViewModel.cs
+++ Quiz/MVVM/ViewModel/SettingsViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Windows.Input;
 using Quiz.Core;
@@ -6,10 +7,16 @@ using Quiz.DataBase;
 
 namespace Quiz.MVVM.ViewModel
 {
-    public class SettingsViewModel : ObservableObject
+    internal class SettingsViewModel : ObservableObject
     {
-        public ICommand ItemChangedCommand;
         private readonly DatabaseTools _questionTools;
+        private string _content = string.Empty;
+        private string _answerA = string.Empty;
+        private string _answerB = string.Empty;
+        private string _answerC = string.Empty;
+        private string _answerD = string.Empty;
+        public ICommand ItemChangedCommand { get; set; }
+
         public SettingsViewModel(DatabaseTools questionTools)
         {
             _questionTools = questionTools;
@@ -18,11 +25,56 @@ namespace Quiz.MVVM.ViewModel
 
         public List<string> QuestionsForView => _questionTools.Questions.Select(q => q.QuestionContent).ToList();
         public int CurrentIndex { get; set; } = -1;
-        public string Content { get; set; } = string.Empty;
-        public string AnswerA { get; set; } = string.Empty;
-        public string AnswerB { get; set; } = string.Empty;
-        public string AnswerC { get; set; } = string.Empty;
-        public string AnswerD { get; set; } = string.Empty;
+
+        public string Content
+        {
+            get => _content;
+            set
+            {
+                _content=value;
+                OnPropertyChanged(nameof(Content));
+            }
+        }
+
+        public string AnswerA
+        {
+            get => _answerA;
+            set
+            {
+                _answerA = value;
+                OnPropertyChanged(nameof(AnswerA));
+            }
+        }
+
+        public string AnswerB
+        {
+            get => _answerB;
+            set
+            {
+                _answerB = value;
+                OnPropertyChanged(nameof(AnswerB));
+            }
+        }
+
+        public string AnswerC
+        {
+            get => _answerC;
+            set
+            {
+                _answerC = value;
+                OnPropertyChanged(nameof(AnswerC));
+            }
+        }
+
+        public string AnswerD
+        {
+            get => _answerD;
+            set
+            {
+                _answerD = value;
+                OnPropertyChanged(nameof(AnswerD));
+            }
+        }
 
         private void ShowSelectedQuestion(object obj)
         {
